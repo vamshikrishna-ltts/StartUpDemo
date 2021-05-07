60262fce3f11c5a83d8e0d2cdadab625bc8abc9d
Vamshikrishna <Vamshikrishna.Bose@Ltts.com>
2021-05-07 14:31:50 +0530

    start up class demo

---
+++ Filters/RequestSetOptionsMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace StartUpClassDemo.Filters
+{
+    public class RequestSetOptionsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestSetOptionsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        // Test with https://localhost:5001/Privacy/?option=Hello
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var option = httpContext.Request.Query["option"];
+
+            if (!string.IsNullOrWhiteSpace(option))
+            {
+                httpContext.Items["option"] = WebUtility.HtmlEncode(option);
+            }
+
+            await _next(httpContext);
+        }
+    }
+}
